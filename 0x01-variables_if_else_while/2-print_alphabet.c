a0c4395f9403440e3d65ba4834b525c81517728b
Scarlet99omnia <omniaahdaoui@gmail.com>
2023-04-24 12:14:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase,
+ *followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 char alp[26] = "yfjggjhiljoljlklj;
+	 int i;
+	 for (i = '0'; i <= '26'; i++)
+{
+	putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
