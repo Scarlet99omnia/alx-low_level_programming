b85bb0cbccea682cd596400ba58c1776b7c6ddde
Scarlet99omnia <omniaahdaoui@gmail.com>
2023-04-24 13:51:06 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	 char alp[26] = "abcdefghijklmnopqrstuvwxyz;
-	 int i;
-	 for (i = '0'; i < '26'; i++)
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = '0'; i < 26; i++)
 {
 	putchar(alp[i]);
 }
