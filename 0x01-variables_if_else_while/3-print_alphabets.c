8a35ad3396d9461a7dee8c35dd3431b92b7848e3
Scarlet99omnia <omniaahdaoui@gmail.com>
2023-04-24 16:46:48 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet
+*
+* Return: Aways 0 (Sucess)
+*/
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ;
+int i:
+	for ( i = 0; i ＜ 52;= i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
