41df78a7e3b10bfc5356094552b3a2e6d4414096
Scarlet99omnia <omniaahdaoui@gmail.com>
2023-04-25 15:46:33 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchat
+ * is written  by mbah
+ * Return: 0
+ */
+
+int main(void)
+｛
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+｝
