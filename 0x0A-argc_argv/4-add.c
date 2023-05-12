91e2f6930d0ad704344550e48294d4f95512b29d
Scarlet99omnia <omniaahdaoui@gmail.com>
2023-05-12 09:00:33 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -31,3 +31,27 @@ return (1);
 * Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
+{
+/*Declaring variables*/
+int count;
+int str_to_int;
+int sum = 0;
+count = 1;
+while (count < argc) /*Goes through the whole array*/
+{
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
+}
+/*Condition if one of the number contains symbols that are not digits*/
+else
+{
+printf("Error\n");
+return (1);
+}
+count++;
+}
+printf("%d\n", sum); /*print sum*/
+return (0);
+}
